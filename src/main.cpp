4f5e15d4cdd972f01a6b2c809a1d7adbee2d5559
Schultzie101 <totu.alt@gmail.com>
2026-07-30 15:51:37 -0700

    fixed bugs now have more bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 //--INITIAILZE--
 #include <Arduino.h>
+#include <TimerOne.h>
+
 int directionPin = 13;
 int pwmPin = 11;
 int brakePin = 8;
 float frequency_currrent; 
 int led_brightness;
+
+
+
 //--FLOW SENSOR VARIABLES--
-volatile double volume;
 float lastTime = 0;
 int sensorPin = 2;
-float flowrate = 0;
+volatile double volume = 0.0;
+volatile double flowrate = 0.0;
 int time_milli = 500; //millis
 float flow_average = 0;
+const double TIME_INTERVAL = 0.1; // seconds
+bool timer_started = false; 
+
+//--BUTTON VARIABLES 
+int buttonPin = 7;
+int value = 0;
 
 //Note: Larger "array_size" means more points for the "frequency_array" to calculate. 
 //This leads to a smoother live graph...
@@ -47,55 +58,86 @@ float array_average(float array [], int size4array){
 }
 
 
+
+
+// This function runs automatically every TIME_INTERVAL
+void calculate_slope() {
+  static double previous_volume = 0; // Remembers value between interrupts (only sets to 0 the first time it's run)
+
+  double current_volume = volume;
+
+  // slope = rise / run = change in Y / change in X
+  flowrate = (current_volume - previous_volume) / TIME_INTERVAL;
+  previous_volume = current_volume;
+  flowrate = volume * 60;
+}
+
+
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
 return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void setup() {
+
   Serial.begin(115200);
+  //--BUTTON SETUP--
+  pinMode(buttonPin, INPUT_PULLUP);
+
   //initialize LED digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(directionPin, OUTPUT);
     pinMode(brakePin, OUTPUT);
-  volume = 0;
   attachInterrupt(digitalPinToInterrupt(sensorPin), pulse, RISING);  //DIGITAL Pin 3: Interrupt 0
-  }
+  
+
+  Timer1.initialize((int) TIME_INTERVAL * 1000000); // takes in us (microseconds)
+  Timer1.attachInterrupt(calculate_slope);
+  Timer1.stop();
+}
 
 
 void loop() { 
-
+    double flowrate_copy;
   digitalWrite(LED_BUILTIN, LOW);
   if (items_pointer-items_pointer_beginning == SIZE * __SIZEOF_DOUBLE__){
+    if(!timer_started){
+      Timer1.start();
+      timer_started = true;
+    } 
     //We have now loaded all the data
     //turn on main led
     digitalWrite(LED_BUILTIN, HIGH);
     // Change direction. High = clockwise LOW= counterclockwise
-    digitalWrite(directionPin, HIGH);
+    // BUTTON DIRECTION
+    value = digitalRead(buttonPin);
     digitalWrite(brakePin, LOW);
+    if(value == 1){
+      digitalWrite(directionPin, HIGH);
+    }
+    if(value == 0){
+      digitalWrite(directionPin, LOW);
+    }
+
+
     for (int index = 0; index < SIZE; index++){
-        // Convert volume to flow rate
-      if (millis() - lastTime > time_milli){
-        flowrate = volume * 60 * 2;
-        lastTime = millis();
-        volume = 0;
-      }
       led_brightness = round(mapFloat(items[index], 0.00,1.00,minimum_pwm,255.00));
       analogWrite(pwmPin, led_brightness );
-      //Serial.println(led_brightness);
-      flow_array[flow_index] = flowrate;
+      noInterrupts();
+      flowrate_copy = flowrate;
+      interrupts();
+
+      flow_array[flow_index] = flowrate_copy;
       flow_index = flow_index + 1;
       flow_average = array_average(flow_array, array_size);
       if (flow_index >= array_size) {
       flow_index = 0;
       }
-
       Serial.println(flow_average);
-      delay(50);
+      delay(200);
     }
   }
   else {
   if (Serial.available() >= buffer_size){
-    // Serial.readBytes(reinterpret_cast<uint8_t*>(items), buffer_size);
     Serial.readBytes(items_pointer, buffer_size);
     items_pointer += buffer_size;
   }
